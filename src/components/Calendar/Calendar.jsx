98873fb77b192d49d23b69d27c2821a9543e2616
Ayub <xyzkhn0@gmail.com>
2026-04-08 18:36:41 +0530

    added constants

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,31 +1,7 @@
 import React, { useState } from 'react';
 import './Calendar.css';
 import { useCalendar } from '../../hooks/useCalendar';
-
-import imgJan from '../../assets/images/winter.png';
-import imgFeb from '../../assets/images/feb.png';
-import imgMar from '../../assets/images/mar.png';
-import imgApr from '../../assets/images/spring.png';
-import imgMay from '../../assets/images/may.png';
-import imgJun from '../../assets/images/jun.png';
-import imgJul from '../../assets/images/jul.png';
-import imgAug from '../../assets/images/aug.png';
-import imgSep from '../../assets/images/sep.png';
-import imgOct from '../../assets/images/oct.png';
-import imgNov from '../../assets/images/nov.png';
-import imgDec from '../../assets/images/dec.png';
-
-const MONTH_IMAGES = [
-  imgJan, imgFeb, imgMar, imgApr, imgMay, imgJun, 
-  imgJul, imgAug, imgSep, imgOct, imgNov, imgDec
-];
-
-const MONTH_NAMES = [
-  'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE',
-  'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'
-];
-
-const WEEKDAYS = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+import { MONTH_IMAGES, MONTH_NAMES, WEEKDAYS } from '../../constants/calendar';
 
 const Calendar = () => {
   const {
